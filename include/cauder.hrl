6c5b7148f6631a8302aa40c0d48368c612d5d9e8
sadamoto yuna <wv.30h.7389@s.thers.ac.jp>
2026-05-13 21:47:18 +0900

    Delete include directory

--- include/cauder.hrl
+++
@@ -1,7 +0,0 @@
-%%%=============================================================================
-%%% App Information
-%%%=============================================================================
-
--define(APP_NAME, "CauDEr").
--define(APP_URL, "https://github.com/mistupv/cauder").
--define(APP_DB, 'cauder/database').
